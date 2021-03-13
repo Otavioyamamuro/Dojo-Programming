8f9ab39adf24ffa0a864bdbf34ccafef986fdd76
otavio <otavio.yamamuro.slice@estadao.com>
2021-03-13 12:46:19 -0300

    ultimas config laravel

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit1f72207934f280f819d51b7181b2c1be
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1f72207934f280f819d51b7181b2c1be', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit1f72207934f280f819d51b7181b2c1be', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit1f72207934f280f819d51b7181b2c1be::getInitializer($loader));
         } else {
