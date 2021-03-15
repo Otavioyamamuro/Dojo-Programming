8b90d2719065001cc915fdba554eb71398302abb
samira360 <samirablv360@gmail.com>
2021-03-15 20:17:21 -0300

    CSS atualizado e js modifcado

--- public/js/script.js
+++ public/js/script.js
@@ -2,78 +2,123 @@ $.get('http://localhost:8080/carros/', function(data) {
 
     $.each(data,function( index,item  ) {
 
-        $(".container").append("<div class=\"card\">\n" +
-            "            <div class=\"info\">\n" +
-            "                <label >"+item+"</label>\n" +
-            "                <label >"+index+"</label>\n" +
-            "            </div>\n" +
-            "\n" +
-            "            <div class=\"button\">\n" +
-            "                <button>Apagar</button>\n" +
-            "                <button>Editar</button>\n" +
-            "            </div>\n" +
-            "        </div>");
+        $(".div-cards").append("<section class=\"card\">"+
+                "<div class=\"info\">"+
+                "<label>"+item+"</label>"+
+                "<label>"+index+"</label>"+
+            "</div>"+
+                "<div class=\"div-button\">"+
+                    "<button onclick=\"fnExcluir("+id+")\">"+//ver o que passar para ter base para excluir
+                        "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-trash-fill\" viewBox=\"0 0 16 16\">"+
+                        "<path d=\"M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z\"/>"+
+                        "</svg>"+
+                    "</button>"+
+                    "<button onclick=\"fnEditar('"+index+"','"+item+"',"+id+")\">"+//ver o que passar para ter base para editar
+                        "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">"+
+                        "<path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>"+
+                        "<path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>"+
+                        "</svg>"+
+                    "</button>"+
+                "</div>"+
+            "</section>");
     });
 });
 
-document.querySelector("#btnbusca").addEventListener("click",()=>{
-
+$("#btnbusca").click(function(){
     var busca =  $('#busca').val();
 
     $.get('http://localhost:8080/carros/'+busca+'/', function(data) {
 
         $.each(data,function( index,item  ) {
-            $(".container").html("<div class=\"card\">\n" +
-                "            <div class=\"info\">\n" +
-                "                <label >"+item+"</label>\n" +
-                "                <label >"+index+"</label>\n" +
-                "            </div>\n" +
-                "\n" +
-                "            <div class=\"button\">\n" +
-                "                <button>Apagar</button>\n" +
-                "                <button>Editar</button>\n" +
-                "            </div>\n" +
-                "        </div>");
+            $(".div-cards").html("<section class=\"card\">"+
+            "<div class=\"info\">"+
+                "<label>"+item+"</label>"+
+                "<label>"+index+"</label>"+
+            "</div>"+
+                "<div class=\"div-button\">"+
+                    "<button onclick=\"fnExcluir("+id+")\">"+//ver o que passar para ter base para excluir
+                        "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-trash-fill\" viewBox=\"0 0 16 16\">"+
+                           "<path d=\"M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z\"/>"+
+                        "</svg>"+
+                    "</button>"+
+                    "<button onclick=\"fnEditar('"+index+"','"+item+"',"+id+")\">"+//ver o que passar para ter base para editar
+                        "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">"+
+                        "<path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>"+
+                        "<path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>"+
+                        "</svg>"+
+                    "</button>"+
+                "</div>"+
+            "</section>");
         });
     })
 
 })
 
-document.querySelector("#cadastrar").addEventListener("click",()=>{
-    var popup = document.querySelector(".popup");
+$( "#btnAdicionar" ).click(function(){
+    $(".bg").css("display","flex");
+    $('body').css("overflow","hidden");
+    $('html,body').scrollTop(0);
+    $( "#btnInserir" ).text("Cadastrar");
+    $("#modelo").val("");
+    $("#marca").val("");
 
-    popup.style.display = "flex";
 });
 
+$("#btnSair").click(function(){
+    $(".bg").css("display","none");
+    $('body').css("overflow","initial");
+})
 
-$( "#btnCadastrar" ).click(function() {
+function fnExcluir(id){
+    if(confirm("Deseja realmente excluir esse registro?")){
+        alert("foi excluido o registro de id " + id);//apagar essa linha depois
+        /*Colocar codigo o aqui!!!*/
+    }
+}
 
-    var modelo = $("#modelo")
-    var marca = $("#marca")
+function fnEditar(marca,modelo,id){
+    $('html,body').scrollTop(0);
+    $(".bg").css("display","flex");
+    $('body').css("overflow","hidden");
+    $("#btnInserir").text("Editar");
 
-    if(marca.val() !=0 && modelo.val() !=""){
+    $("#modelo").val(modelo);
+    $("#marca").val(marca);
+};
 
-        $.post('http://localhost:8080/carros/', {
-            modelo: modelo.val(),
-            marca: marca.val(),
-        }, function(data) {
 
-            if(data == "sucesso"){
-                alert("Cadastrado com sucesso");
-                modelo.val("");
-                marca.val("");
+$("#btnInserir").click(function() {
+    
+    var modelo = $("#modelo");
+    var marca = $("#marca");
 
-            }else{
-                alert(data)
-            }
-        })
+    if(marca.val() != "" && modelo.val() !=""){
+
+        if($("#btnInserir").text() == "Editar"){
+            alert("vai editar");//apagar essa linha depois
+            //aqui ele vai editar
+
+        }else{
+            alert("vai cadastrar");//apagar essa linha depois
+            //aqui ele vai cadastrar
+
+            $.post('http://localhost:8080/carros/', {
+                    modelo: modelo.val(),
+                    marca: marca.val(),
+                }, function(data) {
+
+                if(data == "sucesso"){
+                    alert("Cadastrado com sucesso");
+                    modelo.val("");
+                    marca.val("");
+
+                }else{
+                    alert(data);
+                }
+            })
+        }
+        
     }else{
         alert("Preencha todos os campos!");
     }
 });
-
-
-
-
-
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,54 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Test-dev Estadão</title>
 
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
 </head>
 <body>
 
     <header>
-        <div><button id="cadastrar">+</button></div>
-        <div class="pesquisa">
-            <input type="text" id="busca" placeholder="Pesquisar aqui">
-            <button id="btnbusca">Pesquisar</button>
+        <div class="container div-header">
+            <div class="div-logo">
+                <img src="../../public/img/logoEstadao.png" alt="">
+            </div>
+            <div class="items-header">
+                <div class="div-add">
+                    <button id="btnAdicionar">
+                        <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="div-pesquisa">
+                    <input type="text" id="busca" placeholder="Pesquisar aqui">
+                    <button id="btnbusca">
+                        <svg class="bi bi-search" viewBox="0 0 16 16" fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                            d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z" />
+                            <path fill-rule="evenodd"
+                            d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
-
     </header>
 
+    <main class="container">
+        <div class="div-cards">
+            <section class="card">
+                <div class="info">
+                    <label id="lblMarca">marca</label>
+                    <label id="lblModelo">modelo</label>
+                </div>
+                <div class="div-button">
+                    <button onclick="fnExcluir(2)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </button>
 
+                    <button onclick="fnEditar('Fiat','Argo')">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                        </svg>
+                    </button>
+                </div>
+            </section>
+        </div>
+    </main>
 
-    <div class="container">
-
-
-</div>
-
-<div class="bg">
-    <div class="popup">
-        <input type="text" id="marca">
-        <input type="text" id="modelo">
-
-        <button id="btnCadastrar">Cadastrar</button>
+    <div class="bg">
+        <div class="popup">
+            <div class="div-sair">
+                <div id="btnSair">
+                    +
+                </div>
+            </div>
+            <input type="text" id="marca" placeholder="Marca">
+            <input type="text" id="modelo" placeholder="Modelo">
+            <button id="btnInserir">Cadastrar</button>
+        </div>
     </div>
-</div>
 
-
-
-
-
-
-
-
-
-
-
-
-    <script src="/public/js/jquery.js"></script>
-    <script src="/public/js/script.js"></script>
-
+    <script src="../../public/js/jquery.js"></script>
+    <script src="../../public/js/script.js"></script>
 
 </body>
 </html>
