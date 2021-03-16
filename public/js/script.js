6ba6925100e6c8fbe02e71272bc2048eacd0cbf3
samira360 <samirablv360@gmail.com>
2021-03-16 14:28:42 -0300

    detalhes terminados

--- public/js/script.js
+++ public/js/script.js
@@ -8,13 +8,12 @@ $.get('http://localhost:8080/carros/', function(data) {
                 "<label>"+item["Modelo"]+"</label>"+
             "</div>"+
                 "<div class=\"div-button\">"+
-                    "<button onclick=\"fnExcluir("+item["id"]+")\">"+//ver o que passar para ter base para excluir
+                    "<button onclick=\"fnExcluir("+item["id"]+")\">"+
                         "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-trash-fill\" viewBox=\"0 0 16 16\">"+
                         "<path d=\"M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z\"/>"+
                         "</svg>"+
                     "</button>"+
-                    "<input type='hidden' class='id-btn' id='"+item["id"]+"'>"+
-                    "<button onclick=\"fnEditar('"+item["Marca"]+"','"+item["Modelo"]+"',"+item["id"]+")\">"+//ver o que passar para ter base para editar
+                    "<button onclick=\"fnEditar('"+item["Marca"]+"','"+item["Modelo"]+"',"+item["id"]+")\">"+
                         "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">"+
                         "<path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>"+
                         "<path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>"+
@@ -28,55 +27,52 @@ $.get('http://localhost:8080/carros/', function(data) {
 $("#btnbusca").click(function(){
     var busca =  $('#busca').val();
 
-    $.get('http://localhost:8080/carros/'+busca+'/', function(data) {
+    if(busca > 0 && busca != ""){
+        $.get('http://localhost:8080/carros/'+busca+'/', function(data) {
 
-        $.each(data,function( index,item  ) {
-            $(".div-cards").html("<section class=\"card\">"+
-                "<div class=\"info\">"+
-                "<label>"+item["Marca"]+"</label>"+
-                "<label>"+item["Modelo"]+"</label>"+
-                "</div>"+
-                "<div class=\"div-button\">"+
-                "<button onclick=\"fnExcluir("+item["id"]+")\">"+//ver o que passar para ter base para excluir
-                "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-trash-fill\" viewBox=\"0 0 16 16\">"+
-                "<path d=\"M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z\"/>"+
-                "</svg>"+
-                "</button>"+
-
-                "<button onclick=\"fnEditar('"+item["Marca"]+"','"+item["Modelo"]+"',"+item['id']+");\">"+//ver o que passar para ter base para editar
-                "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">"+
-                "<path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>"+
-                "<path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>"+
-                "</svg>"+
-                "</button>"+
-                "</div>"+
-            "</section>");
-        });
-    })
-
+            $.each(data,function( index,item  ) {
+                $(".div-cards").html("<section class=\"card\" id='carro-"+item["id"]+"'>"+
+                    "<div class=\"info\">"+
+                    "<label>"+item["Marca"]+"</label>"+
+                    "<label>"+item["Modelo"]+"</label>"+
+                    "</div>"+
+                    "<div class=\"div-button\">"+
+                    "<button onclick=\"fnExcluir("+item["id"]+")\">"+
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-trash-fill\" viewBox=\"0 0 16 16\">"+
+                    "<path d=\"M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z\"/>"+
+                    "</svg>"+
+                    "</button>"+
+                    "<button onclick=\"fnEditar('"+item["Marca"]+"','"+item["Modelo"]+"',"+item['id']+");\">"+
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" class=\"bi bi-pencil-square\" viewBox=\"0 0 16 16\">"+
+                    "<path d=\"M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z\"/>"+
+                    "<path fill-rule=\"evenodd\" d=\"M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z\"/>"+
+                    "</svg>"+
+                    "</button>"+
+                    "</div>"+
+                "</section>");
+            });
+        })
+    }else{
+        
+    }
 })
 
 $( "#btnAdicionar" ).click(function(){
     $(".bg").css("display","flex");
     $('body').css("overflow","hidden");
     $('html,body').scrollTop(0);
-    //$( "#btnEditar" ).css("display","none");
+    $("#btnInserir" ).text("Cadastrar")
     $("#modelo").val("");
     $("#marca").val("");
-
 });
 
 $("#btnSair").click(function(){
     $(".bg").css("display","none");
     $('body').css("overflow","initial");
-    /*$( "#btnInserir" ).css("display","block");
-    $( "#btnEditar" ).css("display","block");*/
 })
 
 function fnExcluir(id){
     if(confirm("Deseja realmente excluir esse registro?")){
-        //alert("foi excluido o registro de id " + id);//apagar essa linha depois
-
         $.ajax({
             method: "POST",
             url: "http://localhost:8080/carros/"+id+"/",
@@ -97,11 +93,10 @@ function fnEditar(marca,modelo,id){
     $('html,body').scrollTop(0);
     $(".bg").css("display","flex");
     $('body').css("overflow","hidden");
-    //$("#btnInserir").css("display","none");
+    $("#btnInserir" ).text("Editar")
     $("#modelo").val(modelo);
     $("#marca").val(marca);
     $("#hdd").val(id);
-
 };
 
 
@@ -109,49 +104,40 @@ $("#btnInserir").click(function() {
 
     var modelo = $("#modelo");
     var marca = $("#marca");
+    var id = $("#hdd").val();
 
     if(marca.val() != "" && modelo.val() !=""){
 
+            if($("#btnInserir").text() == "Editar"){
 
-            $.post('http://localhost:8080/carros/', {
-                    modelo: modelo.val(),
-                    marca: marca.val(),
-                }, function(data) {
+                $.ajax({
+                    method: "POST",
+                    url: "http://localhost:8080/carros/"+id+"/",
+                    data: { "id": id, "modelo":modelo.val(), "marca":marca.val(),"_method":'PUT' }
+                }).done(function( msg ) {
+                    console.log(msg);
+                });
 
-                if(data == "sucesso"){
-                    alert("Cadastrado com sucesso");
-                    modelo.val("");
-                    marca.val("");
+            }else{
 
-                }else{
-                    alert(data);
-                }
-            })
+                $.post('http://localhost:8080/carros/', {
+                        modelo: modelo.val(),
+                        marca: marca.val(),
+                    }, function(data) {
+
+                    if(data == "sucesso"){
+                        alert("Cadastrado com sucesso");
+                        modelo.val("");
+                        marca.val("");
+
+                    }else{
+                        alert(data);
+                    }
+                })
+
+            }
+            
     }else{
         alert("Preencha todos os campos!");
     }
 });
-
-$('#btnEditar').click(function (){
-
-    var modelo = $("#modelo").val();
-    var marca = $("#marca").val();
-    var id = $("#hdd").val();
-
-    if (marca != "" && modelo != "") {
-
-        $.ajax({
-            method: "POST",
-            url: "http://localhost:8080/carros/"+id+"/",
-            data: { "id": id, "modelo":modelo, "marca":marca,"_method":'PUT' }
-        }).done(function( msg ) {
-            console.log(msg);
-        });
-
-    } else {
-        alert("Preencha todos os campos!");
-    }
-
-});
-
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test-dev Estadão</title>
 
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
 </head>
 <body>
 
@@ -14,7 +14,7 @@
     <header>
         <div class="container div-header">
             <div class="div-logo">
-                <img src="/public/img/logoEstadao.png" alt="">
+                <img src="../../public/img/logoEstadao.png" alt="">
             </div>
             <div class="items-header">
                 <div class="div-add">
@@ -25,7 +25,7 @@
                     </button>
                 </div>
                 <div class="div-pesquisa">
-                    <input type="text" id="busca" placeholder="Pesquisar aqui">
+                    <input type="number" id="busca" min="0" placeholder="Pesquisar">
                     <button id="btnbusca">
                         <svg class="bi bi-search" viewBox="0 0 16 16" fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg">
@@ -42,6 +42,26 @@
 
     <main class="container">
         <div class="div-cards">
+            <section class="card" id='carro-1'>
+                <div class="info">
+                <label>Marca</label>
+                <label>Modelo</label>
+            </div>
+                <div class="div-button">
+                    <button onclick="fnExcluir(1)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                    </button>
+                    <input type='hidden' class='id-btn' id="1">
+                    <button onclick="fnEditar('Marca','Modelo',1)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                        </svg>
+                    </button>
+                </div>
+            </section>
         </div>
     </main>
 
@@ -56,12 +76,11 @@
             <input type="text" id="modelo" placeholder="Modelo">
             <input type="hidden" id="hdd">
             <button id="btnInserir">Cadastrar</button>
-            <button id="btnEditar">Editar</button>
         </div>
     </div>
 
-    <script src="/public/js/jquery.js"></script>
-    <script src="/public/js/script.js"></script>
+    <script src="../../public/js/jquery.js"></script>
+    <script src="../../public/js/script.js"></script>
 
 
 </body>
